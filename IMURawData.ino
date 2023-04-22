b5db83a0aeca80d13f36766d9177df0f7540d351
rachelhandran <38197877+rachelhandran@users.noreply.github.com>
2023-04-22 01:20:30 -0700

    Update IMURawData.ino

--- IMURawData.ino
+++ IMURawData.ino
@@ -2,8 +2,22 @@
 #include <Adafruit_BNO055.h>
 #include <Wire.h>
 #include <utility/imumaths.h>
+#include <math.h>
 
 // Global constants
+float thetaM;
+float phiM;
+float thetaFold=0;
+float thetaFnew;
+float phiFold=0;
+float phiFnew;
+ 
+float thetaG=0;
+float phiG=0;
+ 
+float dt;
+unsigned long millisOld;
+
 #define BNO055_SAMPLERATE_DELAY_MS (100)
 #define FIVE (5000)
 
@@ -22,33 +36,52 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  uint8_t system, gyros, accel, mg = 0;
-  myIMU.getCalibration(&system, &gyros, &accel,&mg);
-  
-  imu::Vector<3> acc = myIMU.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  imu::Vector<3> gyro = myIMU.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  imu::Vector<3> mag = myIMU.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
-
-  Serial.print(acc.x());
+  uint8_t system, gyro, accel, mg = 0;
+  myIMU.getCalibration(&system, &gyro, &accel, &mg);
+  imu::Vector<3> acc =myIMU.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  imu::Vector<3> gyr =myIMU.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+   
+  thetaM=-atan2(acc.x()/9.8,acc.z()/9.8)/2/3.141592654*360;
+  phiM=-atan2(acc.y()/9.8,acc.z()/9.8)/2/3.141592654*360;
+  phiFnew=.95*phiFold+.05*phiM;
+  thetaFnew=.95*thetaFold+.05*thetaM;
+   
+  dt=(millis()-millisOld)/1000.;
+  millisOld=millis();
+   
+  thetaG=thetaG+gyr.y()*dt;
+  phiG=phiG-gyr.x()*dt;
+   
+  Serial.print(acc.x()/9.8);
   Serial.print(",");
-  Serial.print(acc.y());
+  Serial.print(acc.y()/9.8);
   Serial.print(",");
-  Serial.print(acc.z());
+  Serial.print(acc.z()/9.8);
   Serial.print(",");
-
   Serial.print(accel);
   Serial.print(",");
-  Serial.print(gyros);
+  Serial.print(gyro);
   Serial.print(",");
   Serial.print(mg);
   Serial.print(",");
-  Serial.println(system);
+  Serial.print(system);
+  Serial.print(",");
+  Serial.print(thetaM);
+  Serial.print(",");
+  Serial.print(phiM);
+  Serial.print(",");
+  Serial.print(thetaFnew);
+  Serial.print(",");
+  Serial.print(phiFnew);
+  Serial.print(",");
 
-  //Serial.print(gyro.x());
-  //Serial.print(",");
-  //Serial.print(gyro.y());
-  //Serial.print(",");
-  //Serial.println(gyro.z());
-
+  Serial.print(thetaG);
+  Serial.print(",");
+  Serial.println(phiG);
+   
+  phiFold=phiFnew;
+  thetaFold=thetaFnew;
+   
+  
   delay(BNO055_SAMPLERATE_DELAY_MS);
 }
